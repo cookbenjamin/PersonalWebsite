8ed36f5aad08f329b836767d604f2e8967b32813
cookbenjamin <cookbenjamin@gmail.com>
2016-11-23 23:59:23 -0800

    Added some comments to javascript

--- public/js/js/js.js
+++ public/js/js/js.js
@@ -1,4 +1,17 @@
-function toggleMenu() {
+/**
+ * Constructor for the site object.
+ * All custom code for this site will go under this object
+ */
+function site() {
+
+}
+
+/**
+ * Function to open and close the main navigation menu.
+ * It toggles the classes causing the hamburger to animate,
+ * and the menu to become visible and interactable
+ */
+site.toggleMenu = function() {
     $("#navigation").toggleClass("open");
     $(".btn").toggleClass("open");
-}
+};
